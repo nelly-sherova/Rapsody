2ccf1ba15967e7aef4752896314d86af92476de1
unknown <nellysherova@mail.ru>
2021-04-22 20:34:48 +0500

    Добавила 2 интерфейса IAllFoods и IFoodsCategory

---
+++ Rapsody/Interfaces/IAllFoods.cs
@@ -0,0 +1,15 @@
+﻿using Rapsody.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rapsody.Interfaces
+{
+    public interface IAllFoods
+    {
+        IEnumerable<Food> Foods { get; }
+        IEnumerable<Food> getFavFoods { get; set; }
+        Food getObjectFood(int foodId);
+    }
+}
---
+++ Rapsody/Interfaces/IFoodsCategory.cs
@@ -0,0 +1,13 @@
+﻿using Rapsody.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rapsody.Interfaces
+{
+    public interface IFoodsCategory
+    {
+        IEnumerable<Category> AllFoods { get; }
+    }
+}

635df5b564be5faf25ec4548854d7fcfd4484dbe
unknown <nellysherova@mail.ru>
2021-04-24 12:44:38 +0500

    Добавила View для FoodsController и проверила его работу

--- Rapsody/Controllers/FoodsController.cs
+++ Rapsody/Controllers/FoodsController.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Rapsody.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Rapsody.Controllers
 {
-    public class FoodsController
+    public class FoodsController : Controller
     {
+        private readonly IAllFoods _allFoods;
+        private readonly IFoodsCategory _allCategories;
+
+        public FoodsController (IAllFoods iAllFoods, IFoodsCategory iFoodsCat)
+        {
+            _allFoods = iAllFoods;
+            _allCategories = iFoodsCat;
+        }
+
+        public IActionResult List()
+        {
+            var foods = _allFoods.Foods;
+            return View(foods);
+        }
     }
 }
